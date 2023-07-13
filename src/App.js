8dba86abd1a2a54ea756b218d1ca7429d7ffd6a3
Nalwaddaimmaculate <117144141+Nalwaddaimmaculate@users.noreply.github.com>
2023-07-13 11:18:03 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ToDo from './Components/ToDo';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> ToDo list project</h1>
+     <ToDo/>
+    </div>
+  );
+}
+
+export default App;
